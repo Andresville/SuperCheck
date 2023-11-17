f0b077dd442eac3d22bb6f59b64fd05085674acd
Andresville <104869327+Andresville@users.noreply.github.com>
2023-11-17 15:41:11 -0300

    Pre Entrega 2 Andres Villegas

--- src/InfoProducts.js
+++ src/InfoProducts.js
@@ -7,7 +7,7 @@ const products =
         price: "$ 350,00",
         stock: 10,
         description: "Pure de tomate Arcor por 520gr",
-        urlImage: "/src/assets/imagen/Arcor-pure-de-tomate.svg"
+        urlImage: "../src/assets/imagen/Arcor-pure-de-tomate.svg"
         
       },
       {
